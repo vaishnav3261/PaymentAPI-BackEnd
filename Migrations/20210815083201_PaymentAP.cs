1e4121304cc357cdb6bfea68f260133948f348e5
Vaishnav3261 <vaishnav3261@gmail.com>
2021-08-28 11:38:52 +0530

    My first Commit

---
+++ Migrations/20210815083201_PaymentAP.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class PaymentAP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
